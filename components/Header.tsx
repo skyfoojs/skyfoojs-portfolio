55c05315646b2eaefdcacbe4d29e4dcde715eab0
skyfoojs <skyfoojs@gmail.com>
2025-10-20 23:13:38 +0800

    Update header animation

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,6 +10,7 @@ export default function Header() {
       <motion.div
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
+        transition={{ type: "spring", stiffness: 50, duration: 0.5 }}
         className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white dark:bg-transparent dark:border-none bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
       ></motion.div>
 
@@ -19,6 +20,7 @@ export default function Header() {
             <motion.li
               initial={{ y: -100, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
+              transition={{ type: "spring", stiffness: 50, duration: 0.5 }}
               className="h-3/4 flex justify-center items-center"
               key={link.hash}
             >
