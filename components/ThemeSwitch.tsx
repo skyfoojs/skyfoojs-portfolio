585c09e163bf796a806293c3d88d20f9ba01ca79
SkyFooJS <skyfoojs@gmail.com>
2025-12-14 12:50:28 +0800

    Update theme switch styles, move the button to top right

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -3,16 +3,19 @@
 import { useTheme } from "@/context/theme-context";
 import React from "react";
 import { BsMoon, BsSun } from "react-icons/bs";
+import { motion } from "framer-motion";
 
 export default function ThemeSwitch() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button
-      className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
+    <motion.button
+      className="fixed top-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
+      initial={{ y: -100, x: "-50%", opacity: 0 }}
+      animate={{ y: 0, x: "-50%", opacity: 1 }}
       onClick={toggleTheme}
     >
       {theme === "light" ? <BsSun /> : <BsMoon />}
-    </button>
+    </motion.button>
   );
 }
